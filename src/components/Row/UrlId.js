43f72187274eccdbb44119416b4e16b4cf4ece76
Amal Prasad <amalprasad354@gmail.com>
2022-08-24 12:53:55 +0530

    row logo section cmplted

--- src/components/Row/UrlId.js
+++ src/components/Row/UrlId.js
@@ -1,2 +1,6 @@
 export const sleek='https://play-lh.googleusercontent.com/Fu7O4qKZevtEfWlfO6U4LXR-oc3XygJa-CQ3ANq8reZU48XmZ6eJcLjuyQYo7YAtIQ'
 export const mmt='https://content.jdmagicbox.com/comp/ernakulam/x2/0484px484.x484.170113095819.e1x2/catalogue/multiwood-kochi-ernakulam-polymer-sheet-dealers-cktpolaitc-250.jpg'
+export const asian='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNQTVNKorCD3sNwr2Km-zEN06R5TsqDDBWaXNOJF2HEfE9sVDC5SIHWltWhTxKmawWh7A&usqp=CAU'
+export const ebco=require('./Image/ebco.png')
+export const denwud=require('./Image/denwud.png')
+export const oxywud='https://oxyindia.com/assets/img/oxy-logo.png'
