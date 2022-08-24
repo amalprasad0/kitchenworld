f22cfb7f8eb67746c401f19116646a5ff7f1ac65
Amal Prasad <amalprasad354@gmail.com>
2022-08-24 12:31:45 +0530

    Company complted

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Banner from './components/Banner/Banner';
 import NavBar from './components/NavBar/NavBar';
+import RowSec from './components/Row/RowSec';
 function App() {
   return (
     <div className="App">
       <NavBar/>
       <Banner/>
+      <RowSec/>
     </div>
   );
 }
---
+++ src/components/Row/UrlId.js
@@ -0,0 +1,2 @@
+export const sleek='https://play-lh.googleusercontent.com/Fu7O4qKZevtEfWlfO6U4LXR-oc3XygJa-CQ3ANq8reZU48XmZ6eJcLjuyQYo7YAtIQ'
+export const mmt='https://content.jdmagicbox.com/comp/ernakulam/x2/0484px484.x484.170113095819.e1x2/catalogue/multiwood-kochi-ernakulam-polymer-sheet-dealers-cktpolaitc-250.jpg'
